3b7f8ddbff2d9fe3297520e32e5039a2ca2218af
Renato Albuquerque <renatomalb83@gmail.com>
2023-05-24 00:15:28 -0300

    updated js code

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ const allPage = document.querySelector("body")
 function showMobileMenu() {
     mobileSection.classList.toggle("open")
     allPage.classList.toggle("bgBody")
-    document.querySelector(".mobileMenu").src = "files/images/icon-close-menu.svg"
+    
+    if (mobileSection.classList.contains("open")) {
+        document.querySelector(".mobileMenu").src = "files/images/icon-close-menu.svg"
+    } else {
+        document.querySelector(".mobileMenu").src = "files/images/icon-menu.svg"
+    }
 }
