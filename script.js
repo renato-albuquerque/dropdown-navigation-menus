9f376b90b1c72589117c3d4e84c072fb5259d297
Renato Albuquerque <renatomalb83@gmail.com>
2023-05-24 01:23:01 -0300

    updated js code

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// VARIABLES
+
 const mobileSection = document.querySelector(".mobileSection")
 const allPage = document.querySelector("body")
+const mobileDropdownFeatures = document.querySelector(".features")
+const mobileDropdownCompany = document.querySelector(".company")
+const featuresSubItems = document.querySelector(".ms-dropdownFeatures")
+const companySubItems = document.querySelector(".ms-dropdownCompany")
+const arrowFeatures = document.querySelector(".arrowFeatures")
+const arrowCompany = document.querySelector(".arrowCompany")
+
+// FUNCTIONS
 
 function showMobileMenu() {
     mobileSection.classList.toggle("open")
@@ -11,3 +21,15 @@ function showMobileMenu() {
         document.querySelector(".mobileMenu").src = "files/images/icon-menu.svg"
     }
 }
+
+// EVENTS
+
+mobileDropdownFeatures.addEventListener("click", () => {
+    featuresSubItems.classList.toggle("hide")
+    arrowFeatures.classList.toggle("arrow")
+})
+
+mobileDropdownCompany.addEventListener("click", () => {
+    companySubItems.classList.toggle("hide")
+    arrowCompany.classList.toggle("arrow")
+})
